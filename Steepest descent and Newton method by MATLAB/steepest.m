2d8b30b03422d1abc8961a9fae7c3b2b18f4dce8
Kaifeng Zheng <48105165+kaifengz007@users.noreply.github.com>
2019-03-20 12:58:33 -0700

    Add files via upload
    This work is for someone want to using Newton's method and Steepest descent to find optimization point. Functiontest is the main function where people start to run

---
+++ Steepest descent and Newton method by MATLAB/steepest.m
@@ -0,0 +1,36 @@
+function [f1,x,n,df,Q,alpha]=steepest(f,x0,alpha)
+n = 0;
+r = 0.5;
+c = 1e-4;
+[f1(n+1),df(:,n+1),Q] = autoDQ(f,x0);
+if(abs(norm(df(:,n+1)))<=1e-5)
+    x = x0;
+end
+while abs(norm(df(:,n+1)))>=1
+    %updating X0
+    x(:,n+1) = x0;
+    %updating derivatives
+    p = -df(:,n+1)/norm(df(:,n+1));
+    %updating the X
+    x(:,n+2) = x(:,n+1)+alpha*p;
+   %update f1,df,Q
+    x0 = x(:,n+2);
+    [f1(n+2),df(:,n+2),Q] = autoDQ(f,x0);
+    df(:,n+2)
+    %backtracking
+   while f1(n+2)>f1(n+1)+c*alpha*df(:,n+1)'*p 
+        alpha = r*alpha
+        x(:,n+2) = x(:,n+1)+alpha*p;
+        x0 = x(:,n+2);
+        [f1(n+2),df(:,n+2),Q] = autoDQ(f,x0);
+        if alpha == 0 || n == 1000||(norm(df(:,n+1))==norm(df(:,n+2))&&norm(df(:,n+1))~=0)
+           break;
+        end
+   end
+   if alpha == 0 || n == 1000||(norm(df(:,n+1))==norm(df(:,n+2))&&norm(df(:,n+1))~=0)
+       fprintf("This function can't converge in this situation");
+       break;
+   end
+    %counting times in loop
+    n = n+1;
+end
